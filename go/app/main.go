dd1da206456c4bf479430f1331c895be78a21c77
ayaco0 <zaurusu8569@yahoo.co.jp>
2023-05-12 21:08:04 +0900

    step4-1,2

--- go/app/main.go
+++ go/app/main.go
@@ -10,10 +10,12 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"strconv"
+	"database/sql"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+	_ "github.com/mattn/go-sqlite3" 
 )
 
 const (
@@ -46,17 +48,18 @@ func addItem(c echo.Context) error {
 	newItem.Name = c.FormValue("name")
 	newItem.Category = c.FormValue("category")
 	imagePath := c.FormValue("image")
-	hash := calculateImageHash(imagePath)
+	hash, _ := calculateImageHash(imagePath)
 	newItem.Image = hash
 
 	// Add new item to existing items
-	existingItems := loadItemsFromJSON()
+	existingItems, _ := loadItemsFromDB("")
 	count := len(existingItems.Items)+1
 	newItem.ID = count
 	existingItems.Items = append(existingItems.Items, newItem)
 
-	// Save data to JSON
-	saveItemFromJSON(existingItems)
+	// Save data to JSON/DB
+	// saveItemToJSON(existingItems)
+	saveItemToDB(newItem)
 
 	c.Logger().Infof("Receive item: %s", newItem.Name)
 	message := fmt.Sprintf("item received: %s", newItem.Name)
@@ -81,31 +84,44 @@ func getImg(c echo.Context) error {
 }
 
 func getItem(c echo.Context) error {
-	// Read JSON file
-	items := loadItemsFromJSON()
+	// Load JSON/DB
+	// items, _ := loadItemsFromJSON()
+	items, _ := loadItemsFromDB("")
 	c.Logger().Infof("Get items")
 
 	return c.JSON(http.StatusOK, items)
 }
 
-func loadItemsFromJSON() Items{
+func loadItemsFromJSON() (Items, error) {
 	// Read JSON file
-	data, _ := os.ReadFile("items.json")
-
+	data, err := os.ReadFile("items.json")
+	if err != nil {
+		return Items{}, err
+	}
 	// Parse JSON into Items struct
 	var items Items
-	_ = json.Unmarshal(data, &items)
+	err = json.Unmarshal(data, &items)
+	if err != nil {
+		return Items{}, err
+	}
 
-	return items
+	return items, nil
 }
 
-func saveItemFromJSON(items Items) {
+func saveItemToJSON(items Items) error {
 	// Save data to JSON
-	data, _ := json.Marshal(items)
-	_ = os.WriteFile("items.json", data, 0644)
+	data, err := json.Marshal(items)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile("items.json", data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func calculateImageHash(filePath string) string {
+func calculateImageHash(filePath string) (string, error) {
 	// Read image file
 	imageData, _ := os.ReadFile(filePath)
 
@@ -115,13 +131,13 @@ func calculateImageHash(filePath string) string {
 	// Convert hash to hexadecimal string
 	hashString := hex.EncodeToString(hash[:]) + ".jpeg"
 
-	return hashString
+	return hashString, nil
 }
 
 func getItemByID(c echo.Context) error {
 	targetStr := c.Param("id")
 	targetID, _ := strconv.Atoi(targetStr)
-	items := loadItemsFromJSON()
+	items, _ := loadItemsFromDB("")
 
 	for _, item := range items.Items {
 		if item.ID == targetID {
@@ -130,6 +146,82 @@ func getItemByID(c echo.Context) error {
 	}
 
 	return c.String(http.StatusOK, "Item not found \n")
+}
+
+func saveItemToDB(item Item) error {
+	// Connect DB
+	db, err := sql.Open("sqlite3", "../db/mercari.sqlite3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()	
+	// Insert item to table
+	cmd := "INSERT INTO items (id, name, category, image_name) VALUES ($1, $2, $3, $4)"
+	result, err := db.Exec(cmd, item.ID, item.Name, item.Category, item.Image)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Return value of the lastID
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("LastInsertId: %d\n", id)
+	return nil
+}
+
+func loadItemsFromDB(keyword string) (Items,error) {
+	var items Items
+	// Connect DB
+	db, err := sql.Open("sqlite3", "../db/mercari.sqlite3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	if keyword == "" {
+		// Load all items 
+		cmd := "SELECT * FROM items WHERE id >= $1"
+		rows, err := db.Query(cmd, 0)	
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		for rows.Next() {
+			var item Item
+			err := rows.Scan(&item.ID, &item.Name, &item.Category, &item.Image)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(item.ID, item.Name, item.Category, item.Image)
+			items.Items = append(items.Items, item)
+		}
+	} else {
+		// Search items by keyword
+		cmd := "SELECT * FROM items WHERE name LIKE '%'||$1||'%'"
+		rows, err := db.Query(cmd, keyword)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		for rows.Next() {
+			var item Item
+			err := rows.Scan(&item.ID, &item.Name, &item.Category, &item.Image)
+			if err != nil {
+				log.Fatal(err)
+			}
+			items.Items = append(items.Items, item)
+		}
+	}
+	return items, nil
+}
+
+func searchItem(c echo.Context) error {
+	keyword := c.QueryParam("keyword")
+	items, err := loadItemsFromDB(keyword)
+	if err != nil {
+		return c.String(http.StatusOK, "Item not found \n")
+	}
+	return c.JSON(http.StatusOK, items)
 }
 
 func main() {
@@ -155,7 +247,7 @@ func main() {
 	e.GET("/image/:imageFilename", getImg)
 	e.GET("/items", getItem)
 	e.GET("/items/:id", getItemByID)
-
+	e.GET("/search", searchItem)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
